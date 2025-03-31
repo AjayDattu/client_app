6bcf72538438d1a18c9a15efac72e4359f8dcd3d
Ajay Dattu <dattuajay005@gmail.com>
2025-03-31 09:44:59 +0530

    git fix

--- components/organisms/transport/STransport.js
+++ components/organisms/transport/STransport.js
@@ -29,7 +29,7 @@ const STransport = () => {
       } catch (error) {
         console.error("Error fetching records:", error);
       } finally {
-        setLoading(true);
+        setLoading(false);
       }
     };
 
@@ -38,7 +38,7 @@ const STransport = () => {
 
   const showDeleteModal = (record) => {
     setSelectedRecord(record);
-    setIsModalOpen(false);
+    setIsModalOpen(true);
   };
 
   const handleDelete = async () => {
