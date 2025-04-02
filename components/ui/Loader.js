a0ca1a0c8daae4bd82ec6b7bdb0a48c1e8db6b1e
Ajay Dattu <dattuajay005@gmail.com>
2025-04-02 16:58:56 +0530

    added a fix

--- components/ui/Loader.js
+++ components/ui/Loader.js
@@ -4,7 +4,9 @@ import React from 'react'
 function Loader() {
   return (
     <div className="fixed z-[1000] w-screen h-screen bg-white">
-      <img src="/Loader.gif" alt="Loading..." className="w-48 h-48 m-20 mt-20 " />
+        <div className='flex justify-center align-middle items-center'>
+           <img src="/Loader.gif" alt="Loading..." className="w-48 h-48 m-20 mt-20 " />
+      </div>
     </div>
   )
 }
